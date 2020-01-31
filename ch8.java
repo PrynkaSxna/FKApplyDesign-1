e47bf62cdcc955d1f33a23605f196f29a314e593
PrynkaSxna <54064367+PrynkaSxna@users.noreply.github.com>
2020-01-31 17:55:49 +0530

    Update ch8.java

--- ch8.java
+++ ch8.java
@@ -14,57 +14,62 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 
-class Wheel{
+class Wheel
+{
     private float rim,tire;
 
-    Wheel(float rim,float tire){
+    Wheel(float rim,float tire)
+    {
         this.rim=rim;
         this.tire=tire;
     }
 
 
-    float diameter(){
+    float diameter()
+    {
         return this.rim + (this.tire * 2);
     }
-
 }
 
-class Gear{
-
-
+class Gear
+{
     private  float chainRing,cog,tire,rim;
 
-    Gear(Map<String ,Float> m1){
+    Gear(Map<String ,Float> m1)
+    {
         this.chainRing =m1.get("chainRing");
         this.cog=m1.get("cog");
         this.rim=m1.get("rim");
         this.tire=m1.get("tire");
     }
 
-    float gearInches(){
+    float gearInches()
+    {
         return this.ratio()*(new Wheel(this.rim,this.tire).diameter());
     }
 
-
-    float ratio(){
+    float ratio()
+    {
         return (this.chainRing / (float)this.cog);
     }
 }
 
-class WhellTest extends TestCase {
+class WhellTest extends TestCase 
+{
     @Test
     void testCalculatesDiameter()
     {
         Wheel wheel = new Wheel(26, (float)1.5);
 
-        assertEquals(29, wheel.diameter(),0.01);}
-
+        assertEquals(29, wheel.diameter(),0.01);
+    }
 }
 
-
-class GearTest extends TestCase{
+class GearTest extends TestCase
+{
     @Test
-    public void testCalculatesGearInches(){
+    public void testCalculatesGearInches()
+    {
         Map<String , Float> m1=new HashMap<>();
         m1.put("chainRing",(float)52);
         m1.put("cog",(float)11);
@@ -75,8 +80,10 @@ class GearTest extends TestCase{
     }
 }
 
-public class ex1{
-    public static void main(String args[]){
+public class ex1
+{
+    public static void main(String args[])
+    {
         WhellTest w1=new WhellTest();
         GearTest g1=new GearTest();
         w1.testCalculatesDiameter();
@@ -84,70 +91,72 @@ public class ex1{
     }
 }
 
-
-class Wheel2{
+class Wheel2
+{
     private float rim,tire;
 
-    Wheel2(float rim,float tire){
+    Wheel2(float rim,float tire)
+    {
         this.rim=rim;
         this.tire=tire;
     }
 
-
-    float diameter(){
+    float diameter()
+    {
         return this.rim + (this.tire * 2);
     }
-
 }
 
-class Gear2{
-
-
+class Gear2
+{
     private  float chainRing,cog;
     private Wheel2 w1;
 
-    Gear2(Map<String ,Float> m1,Wheel2 tmp){
+    Gear2(Map<String ,Float> m1,Wheel2 tmp)
+    {
         this.chainRing =(float)m1.get("chainRing");
         this.cog=(float)m1.get("cog");
        this.w1=tmp;
     }
 
-    float gearInches(){
+    float gearInches()
+    {
         return this.ratio()*w1.diameter();
     }
 
-
-    float ratio(){
+    float ratio()
+    {
         return (this.chainRing / (float)this.cog);
     }
 }
 
-class WhellTest2 extends TestCase {
+class WheelTest2 extends TestCase 
+{
     @Test
     void testCalculatesDiameter()
     {
         Wheel2 wheel = new Wheel2(26, (float)1.5);
-
-        assertEquals(29, wheel.diameter(),0.01);}
-
-}
-
-
-class GearTest2 extends TestCase{
-    @Test
-    public void testCalculatesGearInches(){
-        Map<String , Float> m1=new HashMap<>();
-        m1.put("chainRing",(float)52);
-        m1.put("cog",(float)11);
-
-        Gear2 gear= new Gear2(m1,new Wheel2((float)26,(float)1.5));
-        assertEquals(137.1,gear.gearInches(),0.01);
-
+        assertEquals(29, wheel.diameter(),0.01);
     }
 }
 
-public class ex2{
-    public static void main(String args[]){
+class GearTest2 extends TestCase
+{
+    @Test
+    public void testCalculatesGearInches()
+    {
+        Map<String , Float> m1=new HashMap<>();
+        m1.put("chainRing",(float)52);
+        m1.put("cog",(float)11);
+        Gear2 gear= new Gear2(m1,new Wheel2((float)26,(float)1.5));
+        assertEquals(137.1,gear.gearInches(),0.01);
+    }
+}
+
+public class ex2
+{
+    public static void main(String args[])
+    {
         WhellTest2 w1=new WhellTest2();
         GearTest2 g1=new GearTest2();
         w1.testCalculatesDiameter();
@@ -155,69 +164,75 @@ public class ex2{
     }
 }
 
-
-class Wheel3{
+class Wheel3
+{
     private float rim,tire;
 
-    Wheel3(float rim,float tire){
+    Wheel3(float rim,float tire)
+    {
         this.rim=rim;
         this.tire=tire;
     }
 
-
-    float diameter(){
+    float diameter()
+    {
         return this.rim + (this.tire * 2);
     }
 
 }
 
+interface Observer
+{
+    float changed(float a,float b);
+}
 
-interface Observer{
-float changed(float a,float b);}
-
-class Gear3 {
-
-
+class Gear3 
+{
     private float chainRing, cog;
     private Wheel3 w1;
     private Observer observer;
 
-    Gear3(Map<String, Float> m1, Wheel3 tmp, Observer ob) {
+    Gear3(Map<String, Float> m1, Wheel3 tmp, Observer ob) 
+    {
         this.chainRing = (float) m1.get("chainRing");
         this.cog = (float) m1.get("cog");
         this.w1 = tmp;
         this.observer = ob;
     }
 
-    float gearInches() {
+    float gearInches() 
+    {
         return this.ratio() * w1.diameter();
     }
 
-
-    float ratio() {
+    float ratio() 
+    {
         return (this.chainRing / (float) this.cog);
     }
 
-    float setCog(float new_cog) {
+    float setCog(float new_cog)
+    {
         this.cog = new_cog;
        return this.changed();
     }
 
-    void set_chainring(float new_chainring) {
+    void set_chainring(float new_chainring)
+    {
         this.chainRing = new_chainring;
         this.changed();
     }
 
-    float changed() {
-    float b;
+    float changed() 
+    {
+        float b;
         b = observer.changed(chainRing, cog);
         return b;
     }
-
 }
 
 @RunWith(MockitoJUnitRunner.class)
-    class GearTest3 {
+    class GearTest3 
+    {
     @InjectMocks
         Gear3 g1;
         @Mock
@@ -230,16 +245,12 @@ class Gear3 {
             Gear3 gear= new Gear3(m1,new Wheel3((float)10.0,(float)20.0),tmp);
             when(tmp.changed((float)10.0,(float)20.0)).thenReturn((float)30.00);
 
-            //test the add functionality
             assertEquals( (float)30.0, g1.setCog((float)20.0),0.01 );
         }
-
-
     }
 
-
-public class ex3 {
-
+public class ex3 
+{
         public  static  void Main(String args[]){
         GearTest3 t1=new GearTest3();
         t1.setUp();
